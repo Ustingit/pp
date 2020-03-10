ccada44ac814d31d8d88e6624e25ad3f2ec5ff66
Ustin <ustinx@mail.ru>
2020-03-10 22:06:37 +0300

    add chart with 2 lines but with alien content

--- js/main.js
+++ js/main.js
@@ -1,29 +1,171 @@
-let tl = new TimelineLite(),
-    links = document.querySelectorAll('.menu-item'),
-    pIcon = document.querySelectorAll('.plus-icon');
-tl.staggerFromTo(links,0.4,{autoAlpha:0},{autoAlpha:1,ease:Sine.easeInOut},0.2)
-
-links[2].addEventListener('mouseenter',function(){
-  TweenMax.to(pIcon[0],0.15,{rotation:-45,ease:Power1.easeIn})
-  TweenMax.to('#lineGroup_1',0.20,{attr:{'stroke-dashoffset':'8','stroke-dasharray' :'8'},ease:Power1.easeIn})       
-})
-links[2].addEventListener('mouseleave',function(){
-  TweenMax.to(pIcon[0],0.15,{rotation:0,ease:Power1.easeIn})
-  TweenMax.to('#lineGroup_1',0.20,{attr:{'stroke-dashoffset':'0','stroke-dasharray' :'0'},ease:Power1.easeIn})
-})
-links[3].addEventListener('mouseenter',function(){
-  TweenMax.to(pIcon[1],0.15,{rotation:-45,ease:Power1.easeIn})
-  TweenMax.to('#lineGroup_2',0.20,{attr:{'stroke-dashoffset':'8','stroke-dasharray' :'8'},ease:Power1.easeIn})
-})
-links[3].addEventListener('mouseleave',function(){
-  TweenMax.to(pIcon[1],0.15,{rotation:0,ease:Power1.easeIn})
-  TweenMax.to('#lineGroup_2',0.20,{attr:{'stroke-dashoffset':'0','stroke-dasharray' :'0'},ease:Power1.easeIn})
-})
-
-const menuToggle = document.querySelector(".menu span");
-const menu = document.querySelector('.menu');
-menuToggle.addEventListener('click', function(){
-  menu.classList.toggle('open')
-})
 
 
+
+
+
+//chart:
+function buildChart() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+          var myChart = new Chart(ctx, {
+              type: 'bar',
+              data: {
+                  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                  datasets: [{
+                      label: '# of Votes',
+                      data: [12, 19, 3, 5, 2, 3],
+                      backgroundColor: [
+                          'rgba(255, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(255, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(153, 102, 255, 0.2)',
+                          'rgba(255, 159, 64, 0.2)'
+                      ],
+                      borderColor: [
+                          'rgba(255, 99, 132, 1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)'
+                      ],
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero: true
+                          }
+                      }]
+                  }
+              }
+          });
+}
+
+function buildChart2() { 
+  var myLineChart = new Chart(ctx).Line(data);
+  var data = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+          {fillColor: "rgba(220,220,220,0.2)",
+           strokeColor: "rgba(220,220,220,1)",
+           data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {fillColor: "rgba(151,187,205,0.2)",
+           strokeColor: "rgba(151,187,205,1)",
+           data: [28, 48, 40, 19, 86, 27, 90]
+          }
+      ]};
+
+      lineChartData = {}; //declare an object
+lineChartData.labels = []; //add 'labels' element to object (X axis)
+lineChartData.datasets = []; //add 'datasets' array element to object
+
+for (line = 0; line < 4; line++) {
+    y = [];
+    lineChartData.datasets.push({}); //create a new line dataset
+    dataset = lineChartData.datasets[line]
+    dataset.fillColor = "rgba(0,0,0,0)";
+    dataset.strokeColor = "rgba(200,200,200,1)";
+    dataset.data = []; //contains the 'Y; axis data
+
+    for (x = 0; x < 10; x++) {
+        y.push(line + x); //push some data aka generate 4 distinct separate lines
+        if (line === 0)
+            lineChartData.labels.push(x); //adds x axis labels
+    } //for x
+
+    lineChartData.datasets[line].data = y; //send new line data to dataset
+} //for line
+
+ctx = document.getElementById("myChart").getContext("2d");
+myLineChart = new Chart(ctx).Line(lineChartData);
+
+}
+
+function buildChart3() {
+  //https://codepen.io/k3no/pen/pbYGVa
+
+  var canvas = document.getElementById("myChart");
+var ctx = canvas.getContext('2d');
+
+// Global Options:
+Chart.defaults.global.defaultFontColor = 'black';
+Chart.defaults.global.defaultFontSize = 16;
+
+var data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  datasets: [{
+      label: "Stock A",
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: "rgba(225,0,0,0.4)",
+      borderColor: "red", // The main line color
+      borderCapStyle: 'square',
+      borderDash: [], // try [5, 15] for instance
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "black",
+      pointBackgroundColor: "white",
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "yellow",
+      pointHoverBorderColor: "brown",
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: true
+      data: [65, 59, 80, 81, 56, 55, 40, ,60,55,30,78],
+      spanGaps: true,
+    }, {
+      label: "Stock B",
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: "rgba(167,105,0,0.4)",
+      borderColor: "rgb(167, 105, 0)",
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "white",
+      pointBackgroundColor: "black",
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "brown",
+      pointHoverBorderColor: "yellow",
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: false
+      data: [10, 20, 60, 95, 64, 78, 90,,70,40,70,89],
+      spanGaps: false,
+    }
+
+  ]
+};
+
+// Notice the scaleLabel at the same level as Ticks
+var options = {
+  scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                },
+                scaleLabel: {
+                     display: true,
+                     labelString: 'Moola',
+                     fontSize: 20 
+                  }
+            }]            
+        }  
+};
+
+// Chart declaration:
+var myBarChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: options
+});
+}
